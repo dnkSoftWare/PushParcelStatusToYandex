a545ee43fd226bf6e95ccd95ab7e3e895f18e89f
karundm1 <dmitry.karuna@maxipost.ru>
2019-11-11 16:40:31 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using FBirdLib;
+
+namespace YandexPUSH
+{
+    class Parcel
+    {
+        public uint parcel_id;
+        public string parcel_code;
+        public uint push_id;
+        public uint okod;
+
+        public Parcel(uint parcelId)
+        {
+            parcel_id = parcelId;
+            parcel_code = "";
+            push_id = 0;
+            okod = 0;
+        }
+    }
+    class Program
+    {
+        private static List<Parcel> parcels; // Список посылок с изменёнными статусами
+        static void Main(string[] args)
+        {
+            if (GetParcels())
+            {
+
+            }
+
+            Console.ReadKey();
+        }
+
+        private static bool GetParcels()
+        {
+            parcels = new List<Parcel>();
+            
+            using (var fb = new FBird(autoconnect:true))
+            {
+                var _parcels = fb.Select("select out_parcel_id from get_parcel_for_push_1(6);").Enumerate();
+                foreach (var parcel in _parcels)
+                { 
+                    var p = new Parcel(UInt32.Parse(parcel[0].ToString()));
+                    parcels.Add(p);
+                }
+            }
+
+            return (parcels.Count > 0);
+        }
+    }
+}
